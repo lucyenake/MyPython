ef3d3cba165edb4cc0762e54106531dafb69a0f3
enache.luca <enache.luca@yahoo.com>
2023-06-04 09:09:14 +0300

    Descriptions

--- PythonApplication/PythonApplication/Comprehension.py
+++ PythonApplication/PythonApplication/Comprehension.py
@@ -1,6 +1,7 @@
 def myfunction(a):
     return a*2;
 
+# List Comprehension
 num=[1,2,3,4,5,6,7]
 squares=[]
 
@@ -39,3 +40,49 @@ print(myfunction(3))
 
 y=[myfunction(a) for a in range(10)]
 print(y);
+
+# Dictionary Comprehension
+mydict={'a':1, 'b':2, 'c':3, 'd':4}
+
+print(mydict)
+x= mydict.keys()
+print(x)
+x= mydict.values()
+print(x)
+
+x= mydict.items()
+print(x)
+
+new_dict={k:v*2 for (k,v) in mydict.items()}
+print(new_dict)
+
+new_dict={k*2:v for (k,v) in mydict.items()}
+print(new_dict)
+
+mydict={j:j**2 for j in range(10) if j%2==0}
+print(mydict)
+
+feh={'temp1':10,'temp2':20,'temp3':30,'temp4':40,'temp5':50}
+cel=list(map(lambda x: (float(5)/9)*(x-32),feh.values()))
+cel_dic=dict(zip(feh.keys(),cel))
+
+print(cel_dic)
+
+#Set Comprehension
+
+input=[1,2,2,2,3,3,3,3,4,5,5,5,5,5,5,6,6,6,6,6,7,7,7,7,8,8,8,8]
+output=set()
+for var in input:
+    if var%2==0:
+        output.add(var)
+print (output)
+
+output={var for var in input if var%2==0}
+print(output)
+
+#Generator Comprehension
+input =[1,2,3,4,4,4,5,6,7,7,7,7]
+
+output=(var for var in input if var%2==0)
+for var in output:
+    print(var, end='')
