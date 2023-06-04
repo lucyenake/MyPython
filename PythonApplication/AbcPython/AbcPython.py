ecc1a503f303eab6454af0e7321b4d1ede80032c
enache.luca <enache.luca@yahoo.com>
2023-06-04 19:18:14 +0300

    AbcPython

---
+++ PythonApplication/AbcPython/AbcPython.py
@@ -0,0 +1,5 @@
+x=1 #int
+y=2.3 #float
+z=3+3j #complex
+
+print(z)
--- PythonApplication/Descriptors/Descriptors.py
+++ PythonApplication/Descriptors/Descriptors.py
@@ -1,1 +1,31 @@
+class Decriptors:
+    def __init__(self): #constructor       
+        self.__bmi=0
+    def __get__(self,instance, owner):
+        return self.__bmi
+    def __set__(self,instance, value):
+        if isinstance(value,int):
+            print(value)
+        else: raise TypeError("Bmi can only be ineger")
+        if value<0:
+            raise ValueError("Bmi can never be less than zero") 
+        self.__bmi=value
+    def __delete__(self, isinstance):
+        del self.__bmi   
+
+class Person:
+    bmi=Decriptors()
+    def __init__(self, name, age, bmi): #constructor
+        self.name=name
+        self.age=age
+        self.bmi=bmi       
+    def __str__(self):
+        return "{0} age  {1} with a bmi of {2}".format(self.name, self.age, self.bmi)
+
+person1=Person("John",25,17)
+print(person1)
+person2=Person("Matei",25,47)
+print("Last:")
+print(person1)#atentie bmi is changed
+
 
